07474a8ec3ea709b0fd064985c0017c3d1aaeb53
Awa Melvine <melvineawa@gmail.com>
2018-10-06 11:27:26 -0700

    chore(initial) Initialize repository
    Create the basic html structure for the signup and login
    pages.

---
+++ login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="main.css">
+  <title>User verification system PHP - Login</title>
+</head>
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4 offset-md-4 form-wrapper">
+        <h3 class="text-center">Login</h3>
+        <form action="signup.php" method="post">
+          <div class="form-group">
+            <label>Username</label>
+            <input type="text" name="username" class="form-control">
+          </div>
+          <div class="form-group">
+            <label>Password</label>
+            <input type="password" name="password" class="form-control">
+          </div>
+          <div class="form-group">
+            <button type="submit" name="signup-btn" class="btn btn-success pull-right">Sign Up</button>
+          </div>
+        </form>
+
+      </div>
+    </div>
+  </div>
+</body>
+</html>
